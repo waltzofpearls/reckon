3110fa13fe3f9af72284161c2192806d4a9c0431
Rollie Ma <rollie@rollie.dev>
2019-08-16 17:16:07 -0700

    mtls: secured grpc channel (#3)

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
-FROM golang:1.12-alpine AS builder
+FROM golang:1.12-stretch AS builder
 WORKDIR /reckon/
-RUN apk --no-cache add make
 COPY . .
 RUN make
 
 FROM python:3.6-slim-stretch
 WORKDIR /reckon/
 COPY requirements.txt .
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 COPY --from=builder /reckon/reckon .
 CMD ["./reckon"]
--- main.go
+++ main.go
@@ -2,26 +2,45 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"log"
 	"net"
+	"os"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/waltzofpearls/reckon/api"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 func main() {
-	grpcServer := grpc.NewServer()
+	serverCert := os.Getenv("TLS_SERVER_CERT")
+	serverKey := os.Getenv("TLS_SERVER_KEY")
+	rootCA := os.Getenv("TLS_ROOT_CA")
+	serverAddress := os.Getenv("GRPC_SERVER_ADDRESS")
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM([]byte(rootCA)) {
+		log.Fatal("failed to append client certs")
+	}
+	certificate, err := tls.X509KeyPair([]byte(serverCert), []byte(serverKey))
+	tlsConfig := &tls.Config{
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		Certificates: []tls.Certificate{certificate},
+		ClientCAs:    certPool,
+	}
 
 	var server Server
+	serverOption := grpc.Creds(credentials.NewTLS(tlsConfig))
+	grpcServer := grpc.NewServer(serverOption)
 	api.RegisterMetricsServer(grpcServer, server)
 
-	address := ":3003"
-	listen, err := net.Listen("tcp", address)
-	if err != nil {
-		log.Fatalf("could not listen to %s %v", address, err)
-	}
 	log.Println("Server starting...")
+	listen, err := net.Listen("tcp", serverAddress)
+	if err != nil {
+		log.Fatalf("could not listen to %s %v", serverAddress, err)
+	}
 	log.Fatal(grpcServer.Serve(listen))
 }
 
